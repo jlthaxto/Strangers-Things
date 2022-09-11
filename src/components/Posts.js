142fb1460706ecfc74120e2e9a9fb6514af9cc33
jlthaxto <106979967+jlthaxto@users.noreply.github.com>
2022-09-11 16:23:23 -0500

    code push up Sunday

---
+++ src/components/Posts.js
@@ -0,0 +1,56 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Posts = ({ posts }) => {
+  
+  return (
+    <div id='outer div element'>
+    {
+      posts.map((post) => {
+        const {description, location, price, title, _id, isAuthor } = post;
+        return (
+          <div key={_id}>
+            <h3>{title}</h3>
+            <p>Description: {description}</p>
+            <p>Price: {price}</p>
+            <p>Location: {location}</p>
+            {
+              isAuthor ? (
+                <Link to={`/posts/edit-post/${_id}`}>Edit</Link>
+              ) : (
+                <Link to={`/posts/${_id}`}>View</Link>
+              )
+            }
+         
+
+          
+
+            {
+              isAuthor ? (
+                <Link onClick={`/posts/delete-post/${_id, token}`}>Delete Post</Link>
+              ) : (
+                <Link to={`/posts/${_id}`}>View</Link>
+              )
+            }
+
+
+          </div>
+        )
+      })
+    }
+  </div>
+  )
+}
+
+export default Posts;
+
+
+        // {
+        //   isAuthor === true ? (
+        //     <button>You are the author </button>
+        //    : (
+        //     <button></button>
+        //    ) 
+        //   )
+        // }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,145 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Navbar,
+  Posts,
+  Profile,
+  Home,
+  Register,
+  Login,
+  CreatePost,
+  SinglePostView,
+  EditPost,
+  SearchForm
+} from './components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {
+  getPosts,
+  getUserDetails
+} from './api';
+
+const App = () => {
+  
+  const [posts, setPosts] = useState([]);
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState({});
+  
+  const navigate = useNavigate();
+  
+  function logout() {
+    window.localStorage.removeItem('token');
+    setToken('');
+    setUser({});
+  }
+  
+  async function fetchPosts() {
+    const results = await getPosts(token)
+    setPosts(results.data.posts);
+  }
+  
+  async function getMe() {
+    const storedToken = window.localStorage.getItem('token');
+    
+    if (!token) {
+      if (storedToken) {
+        setToken(storedToken);
+      }
+      return;
+    }
+    
+    const results = await getUserDetails(token)
+    if (results.success) {
+      setUser(results.data);
+    } else {
+      console.log(results.error.message);
+    }
+  }
+  
+  useEffect(() => {
+    fetchPosts();
+  }, [token])
+  
+  useEffect(() => {
+    getMe();
+  }, [token])
+  
+  return (
+    <div>
+      <Navbar logout={ logout } token={ token }/>
+      <Routes>
+        <Route 
+          path='/' 
+          element={<Home />} 
+        />
+        <Route 
+          path='/posts' 
+          element={<Posts 
+            posts={posts} 
+          />} 
+        />
+        <Route
+          exact path='/posts/create-post'
+          element={<CreatePost 
+            token={ token } 
+            fetchPosts={ fetchPosts } 
+            navigate={ navigate }
+          /> }
+        />
+        <Route
+          exact path='/posts/edit-post/:postID'
+          element={<EditPost 
+            posts={ posts }
+            token={ token }
+          />}
+        />
+        <Route
+          path='/posts/:postID'
+          element={<SinglePostView 
+            posts={ posts }
+            token={ token }
+          />}
+        />
+        <Route 
+          path='/profile' 
+          element={<Profile user={ user }/>} 
+        />
+        <Route 
+          path='/register' 
+          element={<Register 
+            setToken={ setToken } 
+            token={token} 
+            navigate={navigate} 
+          />} 
+        />
+        <Route
+          path='/login'
+          element={ <Login 
+            setToken={ setToken }
+            navigate={ navigate }
+          />}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+const container = document.querySelector('#container');
+const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/*
+Login
+Registeration
+Posts
+Profile
+Navbar
+AddPost
+*/
